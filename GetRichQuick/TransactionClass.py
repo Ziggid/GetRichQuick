9a763a1941a3fc2fdd8e47bd31e272fb14e3c61b
hschutte <hschutte@deloitte.nl>
2019-08-12 14:11:18 +0200

    Fixed __str__ function for transaction class

--- GetRichQuick/TransactionClass.py
+++ GetRichQuick/TransactionClass.py
@@ -7,5 +7,5 @@ class Transaction:
         self.orderType = orderType
 
     def __str__(self, transactionDate, transactionVolume, transactionPrice, stockId, orderType):
-        'A' + self.orderType + "of" + self.stockId + "of size" + self.transactionVolume + 'is executed on' self.transactionDate + 'for the price of' + self.transactionPrice
+        return 'A' + self.orderType + "of" + self.stockId + "of size" + self.transactionVolume + 'is executed on' + self.transactionDate + 'for the price of' + self.transactionPrice
 
