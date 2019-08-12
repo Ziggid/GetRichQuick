f244b58532fa3f04e48c1fb102c10eadfb7e29f3
BartVerouden <bverouden@deloitte.nl>
2019-08-12 14:39:24 +0200

    no message

--- GetRichQuick.py
+++ GetRichQuick.py
@@ -1,11 +1,12 @@
 from GetRichQuick.TransactionClass import Transaction
 from GetRichQuick.user import User
-from GetRichQuick.Stock import Stock
+#from GetRichQuick.Stock import Stock
 
 
-u = User("bart", 0)
-t = Transaction(1, 100, 1, "google", "buy")
-t2 = Transaction(1, 55, 1, "google", "sell")
+u = User("bart", 10000)
+t = Transaction(1, 100, 10, "google", "buy")
+t2 = Transaction(1, 55, 10, "google", "sell")
+
 u.doTransaction(t)
 u.doTransaction(t2)
 
